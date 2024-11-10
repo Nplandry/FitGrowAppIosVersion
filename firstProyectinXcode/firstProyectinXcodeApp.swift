e713c523b0a651bd30f7fa7b5f5109854947982b
Nicopobletee <nicopobletel@gmail.com>
2024-11-09 23:48:11 -0300

    Initial Commit

---
+++ firstProyectinXcode/firstProyectinXcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstProyectinXcodeApp.swift
+//  firstProyectinXcode
+//
+//  Created by usuario on 09-11-24.
+//
+
+import SwiftUI
+
+@main
+struct firstProyectinXcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
